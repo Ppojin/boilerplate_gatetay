5a77a927552a54031ab07cc81d3f193a1e7bc44e
HyojinHwang <hhjin1814@gmail.com>
2023-08-26 20:58:24 +0900

    initial project

---
+++ src/test/java/com/ppojin/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppojin.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
